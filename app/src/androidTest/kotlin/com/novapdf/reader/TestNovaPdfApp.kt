683c6583fe6f7c9129e4eb90b4ed9b04696de9f1
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-25 11:35:51 +0530

    Fix screenshot harness Hilt test application

--- app/src/androidTest/kotlin/com/novapdf/reader/TestNovaPdfApp.kt
+++ app/src/androidTest/kotlin/com/novapdf/reader/TestNovaPdfApp.kt
@@ -1,7 +1,19 @@
 package com.novapdf.reader
 
+import dagger.hilt.android.internal.testing.TestApplicationComponentManager
+import dagger.hilt.android.internal.testing.TestApplicationComponentManagerHolder
+
 /**
  * Test-only application that preserves [NovaPdfApp]'s startup behaviour while
  * integrating with Hilt's instrumentation runner.
  */
-class TestNovaPdfApp : NovaPdfApp()
+class TestNovaPdfApp : NovaPdfApp(), TestApplicationComponentManagerHolder {
+
+    private val testComponentManager by lazy(LazyThreadSafetyMode.NONE) {
+        TestApplicationComponentManager(this)
+    }
+
+    override fun componentManager(): TestApplicationComponentManager = testComponentManager
+
+    override fun generatedComponent(): Any = testComponentManager.generatedComponent()
+}
